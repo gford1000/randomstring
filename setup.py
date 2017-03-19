4feeac308942829b880d3b415a145c4f2a6aa47c
Geoff Ford <gford1000@me.com>
2017-03-19 16:45:09 +1100

    Completing bare bones package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='randomstring',
+      version='0.1',
+      description='Random string generator (either base62 or base56), of arbitrary length',
+      url='http://github.com/gford1000/randomstring',
+      author='Geoff Ford',
+      author_email='gford1000@hotmail.com',
+      license='MIT',
+      packages=['randomstring'],
+      zip_safe=False)
